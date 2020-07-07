4ab85e83ddc79130dc589b399544ae1de63e698b
Dmytro Chuchukalo <dimachuchukalo@gmail.com>
2020-07-07 18:40:24 +0300

    first task ready

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Profile from './components/Profile/Profile';
+import {
+  name,
+  tag,
+  location,
+  avatar,
+  stats,
+} from './components/Profile/user.json';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Profile
+        name={name}
+        tag={tag}
+        location={location}
+        avatar={avatar}
+        stats={stats}
+      />
+    </>
   );
-}
+};
 
 export default App;
